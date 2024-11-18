bc437d7e6d90071e36cdad362edc115b1ea9ad0d
alexrefshauge <arefsh22@student.aau.dk>
2024-11-18 18:54:04 +0100

    init

--- main.go
+++ main.go
@@ -7,7 +7,25 @@ import (
 )
 
 func main() {
-	day, err := strconv.Atoi(os.Args[1])
+	arg := os.Args[1]
+	if arg == "init" {
+		for d := range 24 {
+			day := d + 1
+			err := os.MkdirAll(fmt.Sprintf("day%d", day), 0755)
+			if err != nil {
+				panic(err)
+			}
+			fileData := fmt.Sprintf("package day%d\n\nfunc run() {\n\n}\n", day)
+			file, err := os.Create(fmt.Sprintf("day%d/solution.go", day))
+			if err != nil {
+				panic(err)
+			}
+			file.Write([]byte(fileData))
+		}
+		return
+	}
+	day, err := strconv.Atoi(arg)
+
 	if err != nil {
 		panic(err)
 	}
